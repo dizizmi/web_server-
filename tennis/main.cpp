9ccaff527a0b91be841c49aea27876487b191746
dizizmi <65617120+dizizmi@users.noreply.github.com>
2025-11-19 13:10:32 +0800

    Initial Commit

---
+++ tennis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tennis
+//
+//  Created by Alysha Hizam on 19/11/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
