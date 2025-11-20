d3fb86eab5208494e227bc75ed29c528ef7931ab
dizizmi <65617120+dizizmi@users.noreply.github.com>
2025-11-20 13:54:29 +0800

    socket object

---
+++ web_server/Networking/simpleSocket.cpp
@@ -0,0 +1,44 @@
+//
+//  simpleSocket.cpp
+//  
+//
+//  Created by Alysha Hizam on 20/11/25.
+//
+
+#include "simpleSocket.hpp"
+
+HDE::simpleSocket::simpleSocket(int domain, int service, int protocol, int port, u_long interface) //passing the int
+{
+    //Define address structure
+    address.sin_family = domain;
+    address.sin_port = htons(port); //set to port bytes for sys to network
+    address.sin_addr.s_addr = htonl(interface);
+    
+    //establish socket
+    sock = socket(domain, service, protocol);
+    connection = connect_network(sock, address);
+    //establish network connection
+    test_connection(connection);
+}
+
+//test connection virtual function
+void HDE::simpleSocket::test_connection(int item_to_test)
+{
+    //confirm the socket/ connection is properly establishd
+    if (item_to_test < 0)
+    {
+        perror("Not connecting");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//getter func
+
+struct sockaddr_in HDE::simpleSocket::get_address()
+{
+    return address;
+}
+int HDE::simpleSocket::get_sock()
+{
+    return connection;
+}
---
+++ web_server/Networking/simpleSocket.hpp
@@ -0,0 +1,40 @@
+//
+//  simpleSocket.hpp
+//  
+//
+//  Created by Alysha Hizam on 20/11/25.
+//
+
+#ifndef simpleSocket_hpp
+#define simpleSocket_hpp
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+
+
+namespace HDE
+{
+class simpleSocket
+{
+private:
+    struct sockaddr_in address; //define address as microvar
+    int sock; //ref to see connection
+    int connection; //ref to see result
+    
+public:
+    //constructor
+    simpleSocket(int domain, int service, int protocol, int port, u_long interface); /* note: type of IP, service requried, protocol used to support socket operation */
+    //virtual func to connect to a netw
+    virtual int connect_network(int sock, struct sockaddr_in address) = 0;
+    //func to test sockets and connections
+    void test_connection(int);
+    //getter func
+    struct sockaddr_in get_address();
+    int get_sock();
+    int get_connection();
+};
+}
+
+#endif /* simpleSocket_hpp */
